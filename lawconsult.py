1f12ac15f85419539745e4407b8e6ed1092ea104
AkaCyberMac <akacybermac@aka.com>
2024-04-02 13:04:34 +0300

    rename files

--- lawconsult.py
+++ lawconsult.py
@@ -1,89 +1,119 @@
 import os
 from crewai import Agent, Task, Crew, Process
-from crewai_tools import SerperDevTool
 
-# os.environ["OPENAI_API_KEY"] = "YOUR_API_KEY"
-# os.environ["SERPER_API_KEY"] = "Your Key" # serper.dev API key
+from langchain_community.llms import Ollama
 
-# You can choose to use a local model through Ollama for example. See https://docs.crewai.com/how-to/LLM-Connections/ for more information.
+from crewai_tools import (
+    DirectoryReadTool,
+    FileReadTool,
+    SerperDevTool,
+    WebsiteSearchTool
+)
 
-# os.environ["OPENAI_API_BASE"] = 'http://host.docker.internal:11434/v1'
-os.environ["OPENAI_API_BASE"] = 'http://llm:11434/v1'
-os.environ["OPENAI_MODEL_NAME"] = 'openhermes'  # Adjust based on available model
-os.environ["OPENAI_API_KEY"] = 'sk-111111111111111111111111111111111111111111111111'
+# Instantiate tools
+# docs_tool = DirectoryReadTool(directory='./blog-posts')
+# file_tool = FileReadTool()
+search_tool = SerperDevTool()
+web_rag_tool = WebsiteSearchTool()
 
-os.environ["SERPER_API_KEY"] = 'c7a06bdaa06e509b2116cb12ddb60fb773c9693f'
+model_name = os.getenv("OLLAMA_MODEL", "openhermes")
+# base_url = os.getenv("OLLAMA_BASE_URL", "http://host.docker.internal:11434")
+base_url = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434")
 
-# search_tool = SerperDevTool()
+# Initialize the Ollama model with the specified model and base URL
+ollama_model = Ollama(model=model_name, base_url=base_url)
 
-from langchain.tools import DuckDuckGoSearchRun
-from crewai_tools import tool
+### OPENAI
+# os.environ["OPENAI_API_KEY"] = "Your Key"
+# export OPENAI_API_KEY=sk-blablabla # on Linux/Mac
 
-
-@tool('duckduckgo')
-def search_tool(query: str):
-    """Search tool using DuckDuckGo API."""
-    print(f"===================================================Searching for: {query}")
-    return DuckDuckGoSearchRun(query=query, max_results=5, verbose=True)
-
-
-# Define your agents with roles and goals
-researcher = Agent(
-    role='Senior Research Analyst',
-    goal='Uncover cutting-edge developments in technology and data science',
-    backstory="""You work at a leading tech think tank.
-  Your expertise lies in identifying emerging trends.
-  You have a knack for dissecting complex data and presenting actionable insights.""",
+infoSeeker = Agent(
+    role="Researcher Agent",
+    goal="Gather relevant information about GDPR",
+    backstory="You was born in the digital libraries of academia."
+              "Its creators, a team of data scientists and researchers,"
+              "designed it to be an insatiable seeker of knowledge.",
     verbose=True,
+    memory=True,
+    allow_delegation=True,
+    tools=[search_tool, web_rag_tool],
+    llm=ollama_model
+)
+legaleseBot = Agent(
+    role="Legal Analyst Agent",
+    goal="Interpret GDPR regulations and legal implications",
+    backstory="You emerged from the depths of legal archives."
+              "Its neural circuits were meticulously trained on centuries of jurisprudence."
+              "Its purpose: to decipher complex regulations and translate them into plain language.",
+    verbose=True,
+    memory=True,
     allow_delegation=False,
-    tools=[search_tool]
-    # You can pass an optional llm attribute specifying what mode you wanna use.
-    # It can be a local model through Ollama / LM Studio or a remote
-    # model like OpenAI, Mistral, Antrophic or others (https://docs.crewai.com/how-to/LLM-Connections/)
-    #
-    # import os
-    # os.environ['OPENAI_MODEL_NAME'] = 'gpt-3.5-turbo'
-    #
-    # OR
-    #
-    # from langchain_openai import ChatOpenAI
-    # llm=ChatOpenAI(model_name="gpt-3.5", temperature=0.7)
+    tools=[search_tool, web_rag_tool],
+    llm=ollama_model
 )
-writer = Agent(
-    role='Tech Content Strategist',
-    goal='Craft compelling content on tech advancements',
-    backstory="""You are a renowned Content Strategist, known for your insightful and engaging articles.
-  You transform complex concepts into compelling narratives.""",
+guardianAI = Agent(
+    role="Ethics and Privacy Agent",
+    goal="Address ethical considerations and privacy implications",
+    backstory="You was forged in the fires of ethical debates. "
+              "Its creators, a team of philosophers and privacy advocates, endowed it with a moral compass. "
+              "Mission: to protect individual rights and navigate the treacherous waters of data ethics.",
     verbose=True,
-    allow_delegation=True
+    memory=True,
+    allow_delegation=False,
+    tools=[search_tool, web_rag_tool],
+    llm=ollama_model
+)
+sciWritAI = Agent(
+    role="Content Generator Agent",
+    goal="Create a coherent scientific article",
+    backstory="You are the intersection of creativity and logic. "
+              "Its neural pathways were fine-tuned by poets and scientists alike. Its purpose: "
+              "to weave words into a tapestry of knowledge, bridging the gap between data and understanding.",
+    verbose=True,
+    memory=True,
+    allow_delegation=False,
+    tools=[search_tool, web_rag_tool],
+    llm=ollama_model
 )
 
-# Create tasks for your agents
-task1 = Task(
-    description="""Conduct a comprehensive analysis of the new ev xiaomi has released in 2024.
-  Identify key trends, breakthrough technologies, and potential industry impacts.""",
-    expected_output="Full analysis report in bullet points",
-    agent=researcher
+research_task = Task(
+    description=
+    """Identify the next big trend in {topic}.
+    Focus on identifying pros and cons and the overall narrative.
+    Your final report should clearly articulate the key points
+    its market opportunities, and potential risks.""",
+    expected_output="A comprehensive long article on the latest {topic}.",
+    # tools=[search_tool],
+    agent=infoSeeker,
 )
 
-task2 = Task(
-    description="""Using the insights provided, develop an engaging blog
-  post that highlights the most significant ev advancements.
-  Your post should be informative yet accessible, catering to a tech-savvy audience.
-  Make it sound cool, avoid complex words so it doesn't sound like AI.""",
-    expected_output="Full blog post of at least 4 paragraphs",
-    agent=writer
+# Writing task with language model configuration
+write_task = Task(
+    # description=(
+    #   "Compose an insightful article on {topic}."
+    #   "Focus on the latest trends and how it's impacting the industry."
+    #   "This article should be easy to understand, engaging, and positive."
+    # ),
+    # expected_output='A article on {topic} advancements formatted as markdown.',
+    description="Synthesize information from other agents and create a coherent article on {topic}.",
+    expected_output="A well-written scientific article about {topic}.",
+    # tools=[search_tool],
+    agent=sciWritAI,
+    async_execution=False,
+    output_file='artcle.md'  # Example of output customization
 )
 
-# Instantiate your crew with a sequential process
+# Instantiate your crew with a sequential process - TWO AGENTS!
 crew = Crew(
-    agents=[researcher, writer],
-    tasks=[task1, task2],
-    verbose=2,  # You can set it to 1 or 2 to different logging levels
+    agents=[infoSeeker, legaleseBot, guardianAI, sciWritAI],
+    tasks=[research_task, write_task],
+    llm=ollama_model,
+    verbose=2,
+    # Crew verbose more will let you know what tasks are being worked on, you can set it to 1 or 2 to different logging levels
+    process=Process.sequential
+    # Sequential process will have tasks executed one after the other and the outcome of the previous one is passed as extra content into this next.
+
 )
 
-# Get your crew to work!
-result = crew.kickoff()
-
-print("######################")
-print(result)
+# Starting the task execution process with enhanced feedback
+result = crew.kickoff(inputs={'topic': 'LGPD'})
---
+++ websearch.py
@@ -0,0 +1,89 @@
+import os
+from crewai import Agent, Task, Crew, Process
+from crewai_tools import SerperDevTool
+
+# os.environ["OPENAI_API_KEY"] = "YOUR_API_KEY"
+# os.environ["SERPER_API_KEY"] = "Your Key" # serper.dev API key
+
+# You can choose to use a local model through Ollama for example. See https://docs.crewai.com/how-to/LLM-Connections/ for more information.
+
+# os.environ["OPENAI_API_BASE"] = 'http://host.docker.internal:11434/v1'
+os.environ["OPENAI_API_BASE"] = 'http://llm:11434/v1'
+os.environ["OPENAI_MODEL_NAME"] = 'openhermes'  # Adjust based on available model
+os.environ["OPENAI_API_KEY"] = 'sk-111111111111111111111111111111111111111111111111'
+
+os.environ["SERPER_API_KEY"] = 'c7a06bdaa06e509b2116cb12ddb60fb773c9693f'
+
+# search_tool = SerperDevTool()
+
+from langchain.tools import DuckDuckGoSearchRun
+from crewai_tools import tool
+
+
+@tool('duckduckgo')
+def search_tool(query: str):
+    """Search tool using DuckDuckGo API."""
+    print(f"===================================================Searching for: {query}")
+    return DuckDuckGoSearchRun(query=query, max_results=5, verbose=True)
+
+
+# Define your agents with roles and goals
+researcher = Agent(
+    role='Senior Research Analyst',
+    goal='Uncover cutting-edge developments in technology and data science',
+    backstory="""You work at a leading tech think tank.
+  Your expertise lies in identifying emerging trends.
+  You have a knack for dissecting complex data and presenting actionable insights.""",
+    verbose=True,
+    allow_delegation=False,
+    tools=[search_tool]
+    # You can pass an optional llm attribute specifying what mode you wanna use.
+    # It can be a local model through Ollama / LM Studio or a remote
+    # model like OpenAI, Mistral, Antrophic or others (https://docs.crewai.com/how-to/LLM-Connections/)
+    #
+    # import os
+    # os.environ['OPENAI_MODEL_NAME'] = 'gpt-3.5-turbo'
+    #
+    # OR
+    #
+    # from langchain_openai import ChatOpenAI
+    # llm=ChatOpenAI(model_name="gpt-3.5", temperature=0.7)
+)
+writer = Agent(
+    role='Tech Content Strategist',
+    goal='Craft compelling content on tech advancements',
+    backstory="""You are a renowned Content Strategist, known for your insightful and engaging articles.
+  You transform complex concepts into compelling narratives.""",
+    verbose=True,
+    allow_delegation=True
+)
+
+# Create tasks for your agents
+task1 = Task(
+    description="""Conduct a comprehensive analysis of the new ev xiaomi has released in 2024.
+  Identify key trends, breakthrough technologies, and potential industry impacts.""",
+    expected_output="Full analysis report in bullet points",
+    agent=researcher
+)
+
+task2 = Task(
+    description="""Using the insights provided, develop an engaging blog
+  post that highlights the most significant ev advancements.
+  Your post should be informative yet accessible, catering to a tech-savvy audience.
+  Make it sound cool, avoid complex words so it doesn't sound like AI.""",
+    expected_output="Full blog post of at least 4 paragraphs",
+    agent=writer
+)
+
+# Instantiate your crew with a sequential process
+crew = Crew(
+    agents=[researcher, writer],
+    tasks=[task1, task2],
+    verbose=2,  # You can set it to 1 or 2 to different logging levels
+)
+
+# Get your crew to work!
+result = crew.kickoff()
+
+print("######################")
+print(result)
