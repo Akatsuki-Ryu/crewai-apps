e378c5935706c6f0f63f7d0c3b87cddf241111f3
AkaboxTerminal <AkaboxTerminal@aka.com>
2024-04-03 21:23:38 +0300

    opt

--- websearchtoolbox/tasks.py
+++ websearchtoolbox/tasks.py
@@ -2,6 +2,7 @@ from crewai import Task
 from agents import websearchtoolboxagentsclass
 from textwrap import dedent
 
+
 class websearchtoolboxtasksclass:
     def __tip_section(self):
         return "If you do your BEST WORK, I'll give you a $10,000 commission!"
@@ -16,11 +17,11 @@ class websearchtoolboxtasksclass:
         {self.__tip_section()}""",
             ),
             expected_output=f"A comprehensive long article on the latest {var1}.",
-            agent=agent, #infoSeeker_agent,
+            agent=agent,  # infoSeeker_agent,
             # tools=[search_tool],
         )
 
-    def task_2_name(self, agent,var1, var2):
+    def task_2_name(self, agent, var1, var2):
         return Task(
             description=dedent(
                 f"""Synthesize information from other agents and create a coherent article on {var1}.
@@ -28,11 +29,8 @@ class websearchtoolboxtasksclass:
             ),
 
             expected_output=f"A well-written scientific article about {var1}.",
-            agent=agent, #sciWritAI_agent,
+            agent=agent,  # sciWritAI_agent,
             # tools=[search_tool],
             async_execution=False,
-            output_file='artcle.md'  # Example of output customization
+            output_file='search-summary.md'  # Example of output customization
         )
-
-
-
