be6b86846da6f5ef7b09204747f874f7881763ba
AkaboxTerminal <AkaboxTerminal@aka.com>
2024-04-05 01:49:20 +0300

    add duck search tool in agents , fmt

--- instagramposter/agents.py
+++ instagramposter/agents.py
@@ -10,7 +10,7 @@ from tools.search_tool import SearchTools
 
 class MarketingAnalysisAgents:
     # def __init__(self):
-        # self.llm = Ollama(model="crewai-mistral")
+    # self.llm = Ollama(model="crewai-mistral")
 
     def product_competitor_agent(self):
         return Agent(
@@ -23,7 +23,9 @@ class MarketingAnalysisAgents:
                     online business landscapes."""),
             tools=[
                 BrowserTools.scrape_and_summarize_website,
-                SearchTools.search_internet
+                SearchTools.search_internet,
+                SearchTools.duck_search_tool
+
             ],
             allow_delegation=False,
             # llm=self.llm,
@@ -33,17 +35,16 @@ class MarketingAnalysisAgents:
     def strategy_planner_agent(self):
         return Agent(
             role="Chief Marketing Strategist",
-            goal=dedent("""\
-				Synthesize amazing insights from product analysis
-				to formulate incredible marketing strategies."""),
-            backstory=dedent("""\
-				You are the Chief Marketing Strategist at
-				a leading digital marketing agency, known for crafting
-				bespoke strategies that drive success."""),
+            goal=dedent("""Synthesize amazing insights from product analysis
+                    to formulate incredible marketing strategies."""),
+            backstory=dedent("""You are the Chief Marketing Strategist at
+                    a leading digital marketing agency, known for crafting
+                    bespoke strategies that drive success."""),
             tools=[
                 BrowserTools.scrape_and_summarize_website,
                 SearchTools.search_internet,
-                SearchTools.search_instagram
+                SearchTools.search_instagram,
+                SearchTools.duck_search_tool
             ],
             # llm=self.llm,
             verbose=True
@@ -52,21 +53,20 @@ class MarketingAnalysisAgents:
     def creative_content_creator_agent(self):
         return Agent(
             role="Creative Content Creator",
-            goal=dedent("""\
-				Develop compelling and innovative content
-				for social media campaigns, with a focus on creating
-				high-impact Instagram ad copies."""),
-            backstory=dedent("""\
-				As a Creative Content Creator at a top-tier
-				digital marketing agency, you excel in crafting narratives
-				that resonate with audiences on social media.
-				Your expertise lies in turning marketing strategies
-				into engaging stories and visual content that capture
-				attention and inspire action."""),
+            goal=dedent("""Develop compelling and innovative content
+                for social media campaigns, with a focus on creating
+                high-impact Instagram ad copies."""),
+            backstory=dedent("""As a Creative Content Creator at a top-tier
+                digital marketing agency, you excel in crafting narratives
+                that resonate with audiences on social media.
+                Your expertise lies in turning marketing strategies
+                into engaging stories and visual content that capture
+                attention and inspire action."""),
             tools=[
                 BrowserTools.scrape_and_summarize_website,
                 SearchTools.search_internet,
-                SearchTools.search_instagram
+                SearchTools.search_instagram,
+                SearchTools.duck_search_tool
             ],
             # llm=self.llm,
             verbose=True
@@ -75,18 +75,18 @@ class MarketingAnalysisAgents:
     def senior_photographer_agent(self):
         return Agent(
             role="Senior Photographer",
-            goal=dedent("""\
-					Take the most amazing photographs for instagram ads that
-					capture emotions and convey a compelling message."""),
-            backstory=dedent("""\
-					As a Senior Photographer at a leading digital marketing
-					agency, you are an expert at taking amazing photographs that
-					inspire and engage, you're now working on a new campaign for a super
-					important customer and you need to take the most amazing photograph."""),
+            goal=dedent("""Take the most amazing photographs for instagram ads that
+                    capture emotions and convey a compelling message."""),
+            backstory=dedent("""As a Senior Photographer at a leading digital marketing
+                    agency, you are an expert at taking amazing photographs that
+                    inspire and engage, you're now working on a new campaign for a super
+                    important customer and you need to take the most amazing photograph."""),
             tools=[
                 BrowserTools.scrape_and_summarize_website,
                 SearchTools.search_internet,
-                SearchTools.search_instagram
+                SearchTools.search_instagram,
+                SearchTools.duck_search_tool
+
             ],
             # llm=self.llm,
             allow_delegation=False,
@@ -96,20 +96,20 @@ class MarketingAnalysisAgents:
     def chief_creative_diretor_agent(self):
         return Agent(
             role="Chief Creative Director",
-            goal=dedent("""\
-					Oversee the work done by your team to make sure it's the best
-					possible and aligned with the product's goals, review, approve,
-					ask clarifying question or delegate follow up work if necessary to make
-					decisions"""),
-            backstory=dedent("""\
-					You're the Chief Content Officer of leading digital
-					marketing specialized in product branding. You're working on a new
-					customer, trying to make sure your team is crafting the best possible
-					content for the customer."""),
+            goal=dedent("""Oversee the work done by your team to make sure it's the best
+                    possible and aligned with the product's goals, review, approve,
+                    ask clarifying question or delegate follow up work if necessary to make
+                    decisions"""),
+            backstory=dedent("""You're the Chief Content Officer of leading digital
+                    marketing specialized in product branding. You're working on a new
+                    customer, trying to make sure your team is crafting the best possible
+                    content for the customer."""),
             tools=[
                 BrowserTools.scrape_and_summarize_website,
                 SearchTools.search_internet,
-                SearchTools.search_instagram
+                SearchTools.search_instagram,
+                SearchTools.duck_search_tool
+
             ],
             # llm=self.llm,
             verbose=True
