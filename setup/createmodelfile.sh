fc4157a988f820acd7585ab3080500974c9934e7
AkaboxTerminal <AkaboxTerminal@aka.com>
2024-04-05 19:52:49 +0300

    create a list in createmodelfile.sh so user can choose from

--- setup/createmodelfile.sh
+++ setup/createmodelfile.sh
@@ -13,14 +13,35 @@ run_model_script() {
     ollama create $custom_model_name -f $model_file
 }
 
-# Check the number of arguments provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 [llama2|mistral|openhermes]"
+# List of available models
+models=("llama2" "mistral" "openhermes")
+
+# Display available models for the user to choose
+echo "Available models:"
+for ((i=0; i<${#models[@]}; i++)); do
+    echo "$((i+1)). ${models[i]}"
+done
+
+# Prompt the user to choose a model
+read -p "Enter the number corresponding to the model you want to run: " choice
+
+# Check if the user input is a valid choice
+if ! [[ "$choice" =~ ^[0-9]+$ ]]; then
+    echo "Invalid input. Please enter a number."
     exit 1
 fi
 
-# Check the argument provided and run the corresponding model script
-case "$1" in
+# Check if the choice is within the range of available models
+if (( choice < 1 || choice > ${#models[@]} )); then
+    echo "Invalid choice. Please select a number within the range."
+    exit 1
+fi
+
+# Get the selected model name based on the user's choice
+selected_model=${models[choice-1]}
+
+# Run the script for the selected model
+case "$selected_model" in
     llama2)
         run_model_script "llama2" "crewai-llama2" "./Llama2Modelfile"
         ;;
@@ -29,9 +50,5 @@ case "$1" in
         ;;
     openhermes)
         run_model_script "openhermes" "crewai-openhermes" "./OpenhermesModelfile"
-        ;;
-    *)
-        echo "Invalid model name. Please choose from: llama2, mistral, openhermes"
-        exit 1
         ;;
 esac
