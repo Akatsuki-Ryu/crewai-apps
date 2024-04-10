3d5decd2e4ef694a8816047b3eace3039c7021e3
AkaCyberMac <akacybermac@aka.com>
2024-04-10 14:28:30 +0300

    add crew class

--- webragsearch/ragsearch.py
+++ webragsearch/ragsearch.py
@@ -7,23 +7,57 @@ from langchain_core.retrievers import BaseRetriever
 from agents import NewsAgentsclass
 from tasks import NewsTasksclass
 
-agentsobj = NewsAgentsclass()
-news_search_agentobj = agentsobj.news_search_agent
-writer_agentobj = agentsobj.writer_agent
-
-tasksobj = NewsTasksclass(news_search_agentobj, writer_agentobj)
-
-news_search_taskobj = tasksobj.create_news_search_task()
-writer_taskobj = tasksobj.create_writer_task()
 
 # 4. Creating Crew
-news_crew = Crew(
-    agents=[news_search_agentobj, writer_agentobj],
-    tasks=[news_search_taskobj, writer_taskobj],
-    process=Process.sequential,
-    # manager_llm=llm
-)
+class NewsCrew:
+    def __init__(self, search_topic):
+        # self.agents = agents
+        # self.tasks = tasks
+        # self.process = process
+        # self.crew = None
+        self.search_topic = search_topic
 
-# Execute the crew to see RAG in action
-result = news_crew.kickoff()
-print(result)
+    def create_crew(self):
+        self.crew = Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            process=self.process,
+            # manager_llm=llm
+        )
+
+    def run(self):
+        # if self.crew is None:
+        #     raise Exception("Crew is not created. Call create_crew() first.")
+
+        agentsobj = NewsAgentsclass()
+        news_search_agentobj = agentsobj.news_search_agent
+        writer_agentobj = agentsobj.writer_agent
+
+        tasksobj = NewsTasksclass(news_search_agentobj, writer_agentobj)
+        news_search_taskobj = tasksobj.create_news_search_task(self.search_topic)
+        writer_taskobj = tasksobj.create_writer_task(self.search_topic)
+
+        news_crew_obj = Crew(
+            agents=[news_search_agentobj, writer_agentobj],
+            tasks=[news_search_taskobj, writer_taskobj],
+            # process=Process.sequential,
+            verbose=True
+        )
+        # news_crew_obj.create_crew()
+        result = news_crew_obj.kickoff()
+        return result
+
+
+if __name__ == "__main__":
+    print("##Running Rag search Crew\n")
+    print('-------------------------------')
+    search_topic = input("Enter the search topic: ")
+    # if there is no imput , setup a default search topic
+    if search_topic == "":
+        search_topic = "Xiaomi's new car"
+    # define a crew object
+    news_crew_obj = NewsCrew(search_topic)
+    # Get your crew to work!
+    result = news_crew_obj.run()
+    print("###################### result ######################")
+    print(result)
