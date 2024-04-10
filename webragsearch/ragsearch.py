2fe113551ce329337808275947120543ef5d1a29
AkaCyberMac <akacybermac@aka.com>
2024-04-10 16:58:55 +0300

    move tools

--- webragsearch/ragsearch.py
+++ webragsearch/ragsearch.py
@@ -4,12 +4,8 @@ from crewai import Agent, Task, Crew, Process
 from langchain_openai import ChatOpenAI
 from langchain_core.retrievers import BaseRetriever
 from langchain_openai import OpenAIEmbeddings
-from langchain.tools import tool
-from langchain_community.document_loaders import WebBaseLoader
-import requests, os
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
-from langchain_community.tools import DuckDuckGoSearchRun
+
+from tools import SearchNewsDB, GetNews, search_tool
 
 embedding_function = OpenAIEmbeddings()
 
@@ -17,67 +13,6 @@ embedding_function = OpenAIEmbeddings()
 # llm = ChatOpenAI(model="gpt-3.5-turbo")
 
 
-# Tool 1 : Save the news articles in a database
-class SearchNewsDB:
-    @tool("News DB Tool")
-    def news(query: str):
-        """Fetch news articles and process their contents."""
-        API_KEY = os.getenv('NEWSAPI_KEY')  # Fetch API key from environment variable
-        base_url = "https://newsapi.org/v2/everything"
-        print(f"Searching for news on query...")
-        print(query)
-
-
-        params = {
-            'q': query,
-            'sortBy': 'publishedAt',
-            'apiKey': API_KEY,
-            'language': 'en',
-            'pageSize': 5,
-        }
-
-        response = requests.get(base_url, params=params)
-        if response.status_code != 200:
-            print("Failed to retrieve news.")
-            return "Failed to retrieve news."
-
-        articles = response.json().get('articles', [])
-        print(f"Found {len(articles)} articles.")
-        all_splits = []
-        for article in articles:
-            # Assuming WebBaseLoader can handle a list of URLs
-            loader = WebBaseLoader(article['url'])
-            docs = loader.load()
-
-            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-            splits = text_splitter.split_documents(docs)
-            all_splits.extend(splits)  # Accumulate splits from all articles
-
-        # Index the accumulated content splits if there are any
-        if all_splits:
-            vectorstore = Chroma.from_documents(all_splits, embedding=embedding_function,
-                                                persist_directory="./chroma_db")
-            retriever = vectorstore.similarity_search(query)
-            print(f"Found {len(retriever)} relevant articles.")
-            print(retriever)
-            return retriever
-        else:
-            print("No content available for processing.")
-            return "No content available for processing."
-
-
-# Tool 2 : Get the news articles from the database
-class GetNews:
-    @tool("Get News Tool")
-    def news(query: str) -> str:
-        """Search Chroma DB for relevant news information based on a query."""
-        vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=embedding_function)
-        retriever = vectorstore.similarity_search(query)
-        return retriever
-
-
-# Tool 3 : Search for news articles on the web
-search_tool = DuckDuckGoSearchRun()
 
 # 2. Creating Agents
 news_search_agent = Agent(
---
+++ webragsearch/tools.py
@@ -0,0 +1,68 @@
+from langchain.tools import tool
+from langchain_community.document_loaders import WebBaseLoader
+import requests, os
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_community.tools import DuckDuckGoSearchRun
+
+# Tool 1 : Save the news articles in a database
+class SearchNewsDB:
+    @tool("News DB Tool")
+    def news(query: str):
+        """Fetch news articles and process their contents."""
+        API_KEY = os.getenv('NEWSAPI_KEY')  # Fetch API key from environment variable
+        base_url = "https://newsapi.org/v2/everything"
+        print(f"Searching for news on query...")
+        print(query)
+
+
+        params = {
+            'q': query,
+            'sortBy': 'publishedAt',
+            'apiKey': API_KEY,
+            'language': 'en',
+            'pageSize': 5,
+        }
+
+        response = requests.get(base_url, params=params)
+        if response.status_code != 200:
+            print("Failed to retrieve news.")
+            return "Failed to retrieve news."
+
+        articles = response.json().get('articles', [])
+        print(f"Found {len(articles)} articles.")
+        all_splits = []
+        for article in articles:
+            # Assuming WebBaseLoader can handle a list of URLs
+            loader = WebBaseLoader(article['url'])
+            docs = loader.load()
+
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+            splits = text_splitter.split_documents(docs)
+            all_splits.extend(splits)  # Accumulate splits from all articles
+
+        # Index the accumulated content splits if there are any
+        if all_splits:
+            vectorstore = Chroma.from_documents(all_splits, embedding=embedding_function,
+                                                persist_directory="./chroma_db")
+            retriever = vectorstore.similarity_search(query)
+            print(f"Found {len(retriever)} relevant articles.")
+            print(retriever)
+            return retriever
+        else:
+            print("No content available for processing.")
+            return "No content available for processing."
+
+
+# Tool 2 : Get the news articles from the database
+class GetNews:
+    @tool("Get News Tool")
+    def news(query: str) -> str:
+        """Search Chroma DB for relevant news information based on a query."""
+        vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=embedding_function)
+        retriever = vectorstore.similarity_search(query)
+        return retriever
+
+
+# Tool 3 : Search for news articles on the web
+search_tool = DuckDuckGoSearchRun()
